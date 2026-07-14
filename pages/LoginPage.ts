6d823d58e58267bf8f8670f9a5838f72faf24b63
Adi19471 <akumatha@gmail.com>
2026-07-14 19:05:43 +0530

    base url add in yaml

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,7 +1,7 @@
 import { Page } from "@playwright/test";
 
 export async function login(page: Page) {
-  await page.goto("http://10.99.99.57/RSR/");
+  await page.goto("/RSR");
   await page.locator('input[name="username"]').fill(process.env.RSR_USERNAME ?? "ADMINRSR");
   await page.locator('input[name="password"]').fill(process.env.RSR_PASSWORD ?? "ADMINRSR");
   await page.locator("button[type=submit]").click();
